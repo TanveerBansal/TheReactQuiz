ed22c4ea02ae2e171d7c53a24a4ce7e7617f6382
TanveerBansal <singhtanveer020@gmail.com>
2024-09-16 14:52:07 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -147,7 +147,7 @@ const staticData = {
 //     // origin: "http://localhost:3000"
 //     origin: "https://the-react-quiz-tan.vercel.app/"
 // }
-// app.use(cors(corsOption))
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
